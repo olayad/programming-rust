3e0badb2c250514a86fa1a8e856f3714e27849ce
Daniel Olaya <daniel.olayam@gmail.com>
2023-02-02 16:58:42 -0800

    wip. mandelbrot

--- dom-log.md
+++ dom-log.md
@@ -1,3 +1,4 @@
 ## 2023.02.02
 - Review of Maldebrot algo
 - Parsing arguments
+- Working through pixel_to_point func
--- snippets/maldelbrot/src/main.rs
+++ snippets/maldelbrot/src/main.rs
@@ -3,9 +3,26 @@
 
 use std::env;
 use std::str::FromStr;
-use num::Complex;
+use num_complex::Complex;
 
 fn main() {
+    let bounds = (100, 200);
+    let pixel = (25, 175);
+    let upper_left = Complex { re: -1.0, im: 1.0 };
+    let lower_right = Complex { re: 1.0, im: -1.0 };
+
+
+    let(width, height) = (lower_right.re - upper_left.re,
+                          upper_left.im - lower_right.im);
+    println!("width:{} height:{}", width, height);
+    let z = Complex {
+        re: upper_left.re + pixel.0 as f64 * width / bounds.0 as f64,
+        im: upper_left.im - pixel.1 as f64 * height / bounds.1 as f64
+    };
+    println!("z:{}", z);
+
+
+
     let args: Vec<String> = env::args().collect();
 
     if args.len() != 5 {
